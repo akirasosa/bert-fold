620e4abbd46cccd2b9985202e735eb041dcd7696
Akira Sosa <akirasosa@gmail.com>
2020-10-22 11:05:39 +0700

    remove debug code

--- src/const.py
+++ src/const.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 
 DATA_DIR = Path('../data')
-# DATA_PROTEIN_NET_DIR = DATA_DIR / 'ProteinNet'
-DATA_PROTEIN_NET_DIR = Path('/mnt/nfs/vfa-red/akirasosa/data/ProteinNet')
+DATA_PROTEIN_NET_DIR = DATA_DIR / 'ProteinNet'
 
 TMP_DIR = Path('../tmp')
 TMP_DIR.mkdir(parents=True, exist_ok=True)
